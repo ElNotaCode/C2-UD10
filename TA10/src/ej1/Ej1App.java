7c9857087e4b21008b47e328e5179143df97911b
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-04-19 11:05:07 +0200

    Ej1 terminado

--- TA10/src/ej1/Ej1App.java
+++ TA10/src/ej1/Ej1App.java
@@ -1,10 +1,52 @@
 package ej1;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+//@Author ElNotaCode (Eloi Martorell) 19/04/2022
+
 public class Ej1App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//genemarmos el numero
+		NumeroAleatorio n = new NumeroAleatorio();
+		int numero = 0;
+		int intentos = 0;
+		//testeo
+		//System.out.println(n);
+		
+		//creamos un bucle
+		boolean checkNumero = false;
+		Scanner sc = new Scanner(System.in);
+		do {
+			//try
+			try {
+				//pedimos el numero
+				System.out.println("Introduce un valor entero del 1 al 500:");
+				numero = sc.nextInt();
+				
+				//miramos si es correcto
+				if(n.getNumero() == numero) {
+					checkNumero = true;
+				}else {
+					System.out.println("¡Incorrecto!");
+					n.setIntentos(++intentos);
+				}
+				
+				//catch
+			} catch (InputMismatchException e) {
+				// DONE: handle exception
+				System.out.println("Tienes que introducir un valor ENTERO!");
+				n.setIntentos(++intentos);
+				sc.next();//http://www.w3api.com/Java/InputMismatchException/
+			}
+			
+		}while(!checkNumero);
+		sc.close();
+		
+		//fin de la partida
+		System.out.println(n);
 	}
 
 }
--- TA10/src/ej1/NumeroAleatorio.java
+++ TA10/src/ej1/NumeroAleatorio.java
@@ -1,5 +1,51 @@
 package ej1;
 
+import java.util.Random;
+
+//@Author ElNotaCode (Eloi Martorell) 19/04/2022
+
 public class NumeroAleatorio {
 
+	//va a tener un numero y un numero de intentos
+	int numero;
+	int intentos;
+	
+	//va a tener unas constantes MIN Y MAX
+	int MIN = 1;
+	int MAX = 500;
+	
+	//va a tener un constructor por defecto donde se genera el numero mediante metodo y intentos se queda en 0;
+	protected NumeroAleatorio() {
+		this.numero = generarNumero();
+		this.intentos = 0;
+	}
+	
+	//va a tener un metodo para hacer random el numero
+	protected int generarNumero() {
+		
+		Random r = new Random();
+		return (r.nextInt(MAX)+MIN);
+	}
+
+	//to string personalizado
+	
+	@Override
+	public String toString() {
+		return "El numero generado es " + numero + ", te ha llevado " + intentos + " intentos.";
+	}
+
+	//getter del numero y de los intentos y setter intentos
+	
+	public int getIntentos() {
+		return intentos;
+	}
+
+	public void setIntentos(int intentos) {
+		this.intentos = intentos;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+	
 }
