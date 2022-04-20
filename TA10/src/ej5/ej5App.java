30e1f4e9495f34a78c932ab87f5828411992d2a4
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-04-20 08:36:51 +0200

    ej5 throws añadido

--- TA10/src/ej5/ej5App.java
+++ TA10/src/ej5/ej5App.java
@@ -1,24 +1,39 @@
 package ej5;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ej5App {
 	
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Introduce un numero de contraseñas a generar:");
-		int n = sc.nextInt();
-		Password passwords[] = new Password[n];
-		boolean esFuerte[] = new boolean[n];
-		System.out.println("Introduce la longitud de las passwords:");
-		n = sc.nextInt();
-		
-		for (int i = 0; i < passwords.length; i++) {
-			passwords[i] = new Password(n);
-			esFuerte[i] = passwords[i].esFuerte();
-			System.out.println(passwords[i].getPassword() + " " + esFuerte[i]);
+		boolean error;
+		do {
+			error = false;
+		try {
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Introduce un numero de contraseñas a generar:");
+			int n = sc.nextInt();
+			Password passwords[] = new Password[n];
+			boolean esFuerte[] = new boolean[n];
+			System.out.println("Introduce la longitud de las passwords:");
+			n = sc.nextInt();
+			
+			for (int i = 0; i < passwords.length; i++) {
+				passwords[i] = new Password(n);
+				esFuerte[i] = passwords[i].esFuerte();
+				System.out.println(passwords[i].getPassword() + " " + esFuerte[i]);
+				error = true;
+			}
+			
+		} catch (InputMismatchException e) {
+			System.out.println("Se ha detectado un valor que no corresponde.");
 		}
+		
+		}while(!error);
+		
+
 	}
 
 }
